ddfa1bb16ae3935761fc9dc5a672847c64992bd7
GiangHa <ngunhutrau9999@gmail.com>
2019-08-27 09:53:45 +0700

    CM 1

---
+++ BinhBaToday/BinhBaToday/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BinhBaToday.Startup))]
+namespace BinhBaToday
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
